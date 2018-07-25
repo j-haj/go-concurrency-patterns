18c600e05c4e6e622aa708fb412d8f9ac994035a
Jeff Hajewski <jeff.hajewski@gmail.com>
2018-07-25 05:53:17 -0700

    Adds mod wrapper. Removes unnecessary select statements

--- generator.go
+++ generator.go
@@ -5,21 +5,19 @@ import (
 	"time"
 )
 
-func GenerateFilter(filterVal int, source <-chan int) <-chan int {
+func Filter(filterVal int, source <-chan int) <-chan int {
 	output := make(chan int)
 	go func() {
 		encounteredFirstVal := false
 		for {
-			select {
-			case v := <-source:
-				if v == filterVal {
-					if !encounteredFirstVal {
-						encounteredFirstVal = true
-						output <- v
-					}
-				} else {
+			v := <-source
+			if v == filterVal {
+				if !encounteredFirstVal {
+					encounteredFirstVal = true
 					output <- v
 				}
+			} else {
+				output <- v
 			}
 		}
 	}()
@@ -27,25 +25,51 @@ func GenerateFilter(filterVal int, source <-chan int) <-chan int {
 	return output
 }
 
+// SimpleGenerator sends `str` to the returned string chan every `delay`
+// milliseconds.
 func SimpleGenerator(str string, delay time.Duration) <-chan string {
 	output := make(chan string)
 	go func() {
 		for {
-			select {
-			case <-time.After(delay * time.Millisecond):
-				output <- str
-			}
+			<-time.After(delay * time.Millisecond)
+			output <- str
+		}
+	}()
+	return output
+}
+
+// SequenceGenerator returns an int channel that sends a sequence of ints
+// starting at 0 every `delay` milliseconds.
+func SequenceGenerator(delay time.Duration) <-chan int {
+	i := 0
+	output := make(chan int)
+	go func() {
+		for {
+			<-time.After(delay * time.Millisecond)
+			output <- i
+			i++
+		}
+	}()
+	return output
+}
+
+func ModWrapper(m int, input <-chan int) <-chan int {
+	output := make(chan int)
+	go func() {
+		for {
+			v := <-input
+			output <- (v % m)
 		}
 	}()
 	return output
 }
 
 func main() {
-	ch := SimpleGenerator("Hello", time.Duration(1000))
+	m := Filter(0, ModWrapper(3, SequenceGenerator(time.Duration(1000))))
 	for {
 		select {
-		case v := <-ch:
-			fmt.Printf("Received: %s\n", v)
+		case v := <-m:
+			fmt.Printf("Received: %d\n", v)
 		}
 	}
 }
